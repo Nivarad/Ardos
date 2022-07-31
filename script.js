5156804d6f2e2e97da6cc6f00731f262d3e1392e
Nivarad <nivarad1@outlook.com>
2022-07-31 18:36:30 +0300

    Add existing project files prior to the push to GitHub.

---
+++ script.js
@@ -0,0 +1,10 @@
+//Smooth Scrolling
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  e.preventDefault();
+
+  // Matching strategy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
